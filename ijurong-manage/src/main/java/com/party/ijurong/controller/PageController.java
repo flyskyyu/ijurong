408b549f2eddab4ce859a3648374a7d620f93f75
yuqin <361636565@qq.com>
2017-05-16 16:16:52 +0800

    后台ui 提交

--- ijurong-manage/src/main/java/com/party/ijurong/controller/PageController.java
+++ ijurong-manage/src/main/java/com/party/ijurong/controller/PageController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 通用的页面跳转Controller
  * 
@@ -16,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class PageController {
 
     @RequestMapping(value = "{pageName}", method = RequestMethod.GET)
-    public String toPage(@PathVariable("pageName") String pageName) {
+    public String toPage(@PathVariable("pageName") String pageName,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) {
         return pageName;
     }
 
--- ijurong-manage/src/main/webapp/index.jsp
+++ ijurong-manage/src/main/webapp/index.jsp
@@ -1,5 +1,12 @@
 <html>
 <body>
 <h2>Hello World!</h2>
+
+<script language="javascript" type="text/javascript">
+    // 以下方式直接跳转
+    window.location.href='/views/admin/login.html';
+    // 以下方式定时跳转
+    //setTimeout("javascript:location.href='hello.html'", 5000);
+</script>
 </body>
 </html>
