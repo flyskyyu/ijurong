c50a90a13ba46957f6cfa1573d42558b188b262b
huating <914483467@qq.com>
2017-05-19 15:40:07 +0800

    easyui版本变更

--- ijurong-manage/src/main/webapp/easyui/plugins/jquery.splitbutton.js
+++ ijurong-manage/src/main/webapp/easyui/plugins/jquery.splitbutton.js
@@ -1,10 +1,10 @@
-﻿/**
- * jQuery EasyUI 1.3.6
+/**
+ * jQuery EasyUI 1.5.2
  * 
- * Copyright (c) 2009-2014 www.jeasyui.com. All rights reserved.
+ * Copyright (c) 2009-2017 www.jeasyui.com. All rights reserved.
  *
- * Licensed under the GPL license: http://www.gnu.org/licenses/gpl.txt
- * To use it on other terms please contact us at info@jeasyui.com
+ * Licensed under the freeware license: http://www.jeasyui.com/license_freeware.php
+ * To use it on other terms please contact us: info@jeasyui.com
  *
  */
 (function($){
@@ -44,6 +44,6 @@ $.fn.splitbutton.parseOptions=function(_a){
 var t=$(_a);
 return $.extend({},$.fn.linkbutton.parseOptions(_a),$.parser.parseOptions(_a,["menu",{plain:"boolean",duration:"number"}]));
 };
-$.fn.splitbutton.defaults=$.extend({},$.fn.linkbutton.defaults,{menu:null,duration:100,cls:{btn1:"m-btn-active s-btn-active",btn2:"m-btn-plain-active s-btn-plain-active",arrow:"m-btn-downarrow",trigger:"m-btn-line"}});
+$.fn.splitbutton.defaults=$.extend({},$.fn.linkbutton.defaults,{plain:true,menu:null,duration:100,cls:{btn1:"m-btn-active s-btn-active",btn2:"m-btn-plain-active s-btn-plain-active",arrow:"m-btn-downarrow",trigger:"m-btn-line"}});
 })(jQuery);
 
